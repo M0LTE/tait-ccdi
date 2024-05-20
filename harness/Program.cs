7eb75630e0f4397dc4389ace56d0e4d6a3fecb21
Tom M0LTE <tom@m0lte.uk>
2024-05-20 22:11:34 +0100

    Hack in a very rough client for the serial port

---
+++ harness/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Text;
+using tait_ccdi;
+
+var radio = new TaitRadio("COM3", 115200);
+
+//Console.WriteLine("Press enter to queue command");
+
+var sw = Stopwatch.StartNew();
+var sb = new StringBuilder();
+while (sw.ElapsedMilliseconds < 15000)
+{
+    //Console.ReadLine();
+    var rssi = await radio.GetRawRssi();
+    //Console.SetCursorPosition(0, 0);
+    //Console.WriteLine(rssi);
+    sb.AppendLine($"{DateTime.Now.ToString("HH:mm:ss.fff")},{rssi}");
+}
+
+File.WriteAllText("C:\\Users\\me\\Desktop\\rssi.csv", sb.ToString());
---
+++ tait-ccdi/TaitRadio.cs
@@ -0,0 +1,115 @@
+﻿using System.Diagnostics;
+using System.IO.Ports;
+using System.Text;
+
+namespace tait_ccdi;
+
+public class TaitRadio
+{
+    private SerialPort serialPort;
+
+    public TaitRadio(string comPort, int baud)
+    {
+        serialPort = new SerialPort(comPort, baud, Parity.None, 8, StopBits.One);
+        serialPort.NewLine = "\r";
+        serialPort.Open();
+
+        _ = Task.Run(RunRadio);
+    }
+
+    private void RunRadio()
+    {
+        List<char> buffer = [];
+
+        while (true)
+        {
+            var i = serialPort.ReadByte();
+
+            if (i == -1)
+            {
+                Console.WriteLine("Quitting");
+                return;
+            }
+
+            byte b = (byte)i;
+
+            if (b == '.')
+            {
+                var sb = new StringBuilder();
+                sb.Append(buffer.ToArray());
+                var output = sb.ToString();
+                buffer.Clear();
+                if (string.IsNullOrWhiteSpace(output))
+                {
+                    continue;
+                }
+
+                if (output.StartsWith("j") && CcdiCommand.TryParse(output, out var command))
+                {
+                    var response = command.AsQueryResponse();
+
+                    if (response.Command == "064")
+                    {
+                        rawRssi = int.Parse(response.Data) / 10.0;
+                    }
+                    else if (response.Command == "063")
+                    {
+                        averagedRssi = int.Parse(response.Data) / 10.0;
+                    }
+                }
+                else if (output.StartsWith("p"))
+                {
+                    continue;
+                }
+                else
+                {
+                    Debugger.Break();
+                }
+            }
+            else
+            {
+                if (b >= 33 && b <= 126)
+                {
+                    buffer.Add((char)b);
+                }
+                else if (b != 13)
+                {
+                    Debugger.Break();
+                }
+            }
+        }
+    }
+
+    double? rawRssi;
+    double? averagedRssi;
+
+    public async Task<double> GetRawRssi()
+    {
+        var command = new QueryCommand(QueryType.Cctm_RawRssi);
+        var cmd = command.ToCommand();
+        serialPort.WriteLine(cmd);
+
+        while (rawRssi == null)
+        {
+            await Task.Delay(1);
+        }
+        double val = rawRssi.Value;
+        rawRssi = null;
+        return val;
+    }
+
+    public async Task<double> GetAveragedRssi()
+    {
+        var command = new QueryCommand(QueryType.Cctm_AveragedRssi);
+        var cmd = command.ToCommand();
+        serialPort.WriteLine(cmd);
+
+        while (averagedRssi == null)
+        {
+            await Task.Delay(1);
+        }
+        double val = averagedRssi.Value;
+        averagedRssi = null;
+        return val;
+    }
+}
