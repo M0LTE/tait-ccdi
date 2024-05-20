e217d2a1d3f4727d22a8706680319e5a6a0e9de1
Tom M0LTE <tom@m0lte.uk>
2024-05-20 22:11:06 +0100

    Add CCDI commands/responses

---
+++ tait-ccdi/CcdiCommand.cs
@@ -0,0 +1,107 @@
+﻿namespace tait_ccdi;
+
+public record struct CcdiCommand
+{
+    public char Ident { get; set; }
+    public int Size { get; set; }
+    public string? Parameters { get; set; }
+    public required string Checksum { get; set; }
+
+    public static bool TryParse(string command, out CcdiCommand ccdiCommand)
+    {
+        if (command.Length < 5)
+        {
+            ccdiCommand = default;
+            return false;
+        }
+
+        ccdiCommand = new CcdiCommand
+        {
+            Checksum = command[^2..],
+            Ident = command[0],
+            Size = Convert.ToInt32(command.Substring(1, 2), 16)
+        };
+
+        if (ccdiCommand.Size > 0)
+        {
+            ccdiCommand.Parameters = command[3..^2];
+        }
+
+        return true;
+    }
+
+    public const int Terminator = 0x0d;
+}
+
+public record struct QueryCommand
+{
+    public QueryCommand(QueryType queryType) : this()
+    {
+        if (queryType == QueryType.Cctm_RawRssi)
+        {
+            Ident = 'q';
+            Command = "5";
+            Data = "064";
+        }
+        else if (queryType == QueryType.Cctm_AveragedRssi)
+        {
+            Ident = 'q';
+            Command = "5";
+            Data = "063";
+        }
+        else
+        {
+            throw new NotSupportedException($"Not supported: {queryType}");
+        }
+
+        Size = Command.Length + Data.Length;
+        var commandWithoutChecksum = $"{Ident}{Size:00}{Command}{Data}";
+        Checksum = CcdiChecksum.Calculate(commandWithoutChecksum);
+    }
+
+    public char Ident { get; set; }
+    public int Size { get; set; }
+    public string Command { get; set; }
+    public string Data { get; set; }
+    public string Checksum { get; set; }
+
+    public readonly string ToCommand() => $"{Ident}{Size:00}{Command}{Data}{Checksum}";
+}
+
+public record struct QueryResponse
+{
+    public char Ident { get; set; }
+    public int Size { get; set; }
+    public string Command { get; set; }
+    public string Data { get; set; }
+    public string Checksum { get; set; }
+
+    public readonly string ToCommand() => $"{Ident}{Size:00}{Command}{Data}{Checksum}";
+}
+
+public static class CcdiCommandExtensions
+{
+    public static QueryResponse AsQueryResponse(this CcdiCommand ccdiCommand) => new()
+    {
+        Ident = ccdiCommand.Ident,
+        Size = ccdiCommand.Size,
+        Command = ccdiCommand.Parameters![..3],
+        Data = ccdiCommand.Parameters![3..],
+        Checksum = ccdiCommand.Checksum
+    };
+}
+
+public enum QueryType
+{
+    ModelAndCcdiVersion,
+    QuerySdm,
+    Version,
+    SerialNumber,
+    Cctm_PaTemperature,
+    Cctm_AveragedRssi,
+    Cctm_RawRssi,
+    Cctm_ForwardPower,
+    Cctm_ReversePower,
+    Gps,
+    Display
+}
---
+++ tests/CcdiCommandTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using tait_ccdi;
+
+namespace tests;
+
+public class CcdiCommandTests
+{
+    [Fact]
+    public void ManualExample()
+    {
+        CcdiCommand.TryParse("s0D050800TESTHi!DA", out var ccdiCommand).Should().BeTrue();
+        ccdiCommand.Ident.Should().Be('s');
+        ccdiCommand.Size.Should().Be(13);
+        ccdiCommand.Parameters.Should().Be("050800TESTHi!");
+        ccdiCommand.Checksum.Should().Be("DA");
+    }
+
+    [Fact]
+    public void PaTempQuery()
+    {
+        CcdiCommand.TryParse("q0450475B", out var ccdiCommand).Should().BeTrue();
+        ccdiCommand.Ident.Should().Be('q');
+        ccdiCommand.Size.Should().Be(4);
+        ccdiCommand.Parameters.Should().Be("5047");
+        ccdiCommand.Checksum.Should().Be("5B");
+    }
+
+    [Fact]
+    public void PaTempResponse()
+    {
+        CcdiCommand.TryParse("j050472331", out var ccdiCommand).Should().BeTrue();
+        ccdiCommand.Ident.Should().Be('j');
+        ccdiCommand.Size.Should().Be(5);
+        ccdiCommand.Parameters.Should().Be("04723");
+        ccdiCommand.Checksum.Should().Be("31");
+
+        var queryResults = ccdiCommand.AsQueryResponse();
+        queryResults.Command.Should().Be("047");
+        queryResults.Data.Should().Be("23");
+    }
+
+    [Fact]
+    public void PowerResponse()
+    {
+        CcdiCommand.TryParse("j06047481F8", out var ccdiCommand).Should().BeTrue();
+        ccdiCommand.Ident.Should().Be('j');
+        ccdiCommand.Size.Should().Be(6);
+        ccdiCommand.Parameters.Should().Be("047481");
+        ccdiCommand.Checksum.Should().Be("F8");
+
+        var queryResults = ccdiCommand.AsQueryResponse();
+        queryResults.Command.Should().Be("047");
+        queryResults.Data.Should().Be("481");
+    }
+}
