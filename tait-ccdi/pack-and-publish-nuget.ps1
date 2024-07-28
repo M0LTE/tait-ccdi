6635ce240c9b0d883ea9b39826648c154eaa92f1
Tom M0LTE <tom@m0lte.uk>
2024-07-28 09:11:21 +0100

    Publish v4

--- tait-ccdi/pack-and-publish-nuget.ps1
+++ tait-ccdi/pack-and-publish-nuget.ps1
@@ -1,2 +1,2 @@
 ﻿dotnet pack
-dotnet nuget push .\bin\Release\m0lte.tait-ccdi.0.0.3.nupkg -k $env:m0lte_nuget_key -s https://api.nuget.org/v3/index.json
+dotnet nuget push .\bin\Release\m0lte.tait-ccdi.0.0.4.nupkg -k $env:m0lte_nuget_key -s https://api.nuget.org/v3/index.json
